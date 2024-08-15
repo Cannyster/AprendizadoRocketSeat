7f5cdefd3a983e039c58364defb2ea02ec15e14a
Pc do serviço <dionathancampos@hotmail.com>
2024-08-15 13:01:22 -0300

    Adicionada a estrutura para abertura do modal - Nova transação

--- dt-money/src/components/Header/index.tsx
+++ dt-money/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog"; //https://www.radix-ui.com/primitives/docs/components/dialog
 import logoImg from "../../assets/logo.svg";
 
 export function Header() {
@@ -7,9 +8,24 @@ export function Header() {
       <HeaderContainer>
         <HeaderContent>
           <img src={logoImg} alt=""></img>
-          <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+          <Dialog.Root>
+            <Dialog.DialogTrigger asChild>
+              <NewTransactionButton>Nova Transação</NewTransactionButton>
+            </Dialog.DialogTrigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay />
+
+              <Dialog.DialogContent>
+                <Dialog.DialogTitle>Nova Transaçõa</Dialog.DialogTitle>
+                <Dialog.DialogClose />
+              </Dialog.DialogContent>
+            </Dialog.Portal>
+          </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
     </header>
   );
 }
+// Portal - ajuda a colocar o conteúdo fora de todas as estruturas em que ele esta, como se fosse um elemento externo ao local de origem

3f88c866f942b685b4657546fc840f9c09124823
Jhonanthan <dionathancampos@hotmail.com>
2024-08-21 09:03:00 -0300

    criada estrutura para recuperar as transações existentes

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
@@ -7,7 +8,29 @@ import {
   TransactionsTable,
 } from "./styles";
 
+interface transaction {
+  id: number;
+  description: String;
+  type: "income" | "outcome";
+  category: String;
+  price: number;
+  CreatedAt: String;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<transaction[]>([]);
+
+  // Async não pode ser feito dentro de useEffect, por isso foi necesário cria em uma função externa para funcionar
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3333/transactions");
+    const data = await response.json();
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
   return (
     <div>
       <Header />
@@ -15,26 +38,23 @@ export function Transactions() {
 
       <TransactionsContainer>
         <SearchForm />
+
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                {" "}
-                <PriceHighLight variant="income">R$ 12.000,00 </PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/200</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburguer</td>
-              <td>
-                {" "}
-                <PriceHighLight variant="outcome"> - R$ 59,00 </PriceHighLight>
-              </td>
-              <td>Alimentos</td>
-              <td>13/04/200</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.CreatedAt}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>

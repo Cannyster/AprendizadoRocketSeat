4b4cbcbdcd0c4d2d7b746d69465012c612e0903d
Jhonanthan <dionathancampos@hotmail.com>
2024-08-23 13:36:49 -0300

    implementando useMemo no calculo do sumário

--- dt-money/src/hooks/useSummary.ts
+++ dt-money/src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { TransactionsContext } from "../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
 
@@ -8,19 +9,25 @@ export function useSummary() {
   });
 
   // como esses valores são usados unicamente dentro do summary, convem manter os calculos exclusivamente aqui.
-  const summary = transactions.reduce(
-    (acc, transactions) => {
-      if (transactions.type === "income") {
-        acc.income += transactions.price;
-        acc.total += transactions.price;
-      } else {
-        acc.outcome += transactions.price;
-        acc.total -= transactions.price;
-      }
+  // use memo adicionado para impedir o calculo de ser refeito, exceto quando algum valor de transactions for alterado
+  // isto e indicado no array de dependências
+  const summary = useMemo(
+    () =>
+      transactions.reduce(
+        (acc, transactions) => {
+          if (transactions.type === "income") {
+            acc.income += transactions.price;
+            acc.total += transactions.price;
+          } else {
+            acc.outcome += transactions.price;
+            acc.total -= transactions.price;
+          }
 
-      return acc;
-    },
-    { income: 0, outcome: 0, total: 0 }
+          return acc;
+        },
+        { income: 0, outcome: 0, total: 0 }
+      ),
+    [transactions]
   );
 
   return summary;

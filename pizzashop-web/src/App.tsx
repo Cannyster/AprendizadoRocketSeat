5c565e3ef7047c7d0b4fd25758f422f14e7dc78f
Jhonanthan <dionathancampos@hotmail.com>
2024-08-27 09:20:30 -0300

    realizado ajuste nas configurações do eslint + prettier

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -1,11 +1,10 @@
 import "./global.css";
-import { router } from "./routes";
 import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 
 export function App() {
   return (
-   
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router} />
--- pizzashop-web/src/pages/app/dashboard.tsx
+++ pizzashop-web/src/pages/app/dashboard.tsx
@@ -1,8 +1,10 @@
 import { Helmet } from "react-helmet-async";
 
-export function Dashboard(){
-    return <>
-        <Helmet title="Dashboard" />
-        <h1>SingIn</h1>
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <h1>SingIn</h1>
     </>
+  );
 }

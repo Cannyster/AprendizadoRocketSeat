8b029fb529763c9c330dabfeb65ed2fab9ae2ace
Jhonanthan <dionathancampos@hotmail.com>
2024-08-29 09:34:14 -0300

    organizando filtros de pedidos

--- pizzashop-web/src/pages/app/orders/orders.tsx
+++ pizzashop-web/src/pages/app/orders/orders.tsx
@@ -1,15 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { ArrowRight, Search, X } from "lucide-react";
 import { Helmet } from "react-helmet-async";
+import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filters";
 
 export function Orders() {
   return (
@@ -19,11 +17,7 @@ export function Orders() {
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
       </div>
       <div className="space-y-2.5">
-        <form action="" className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros:</span>
-          <Input placeholder="Nome Do Cliente" className="h-8 w-[320px]" />
-        </form>
-
+        <OrderTableFilters />
         <div className="rounded-md border">
           <Table>
             <TableHeader>
@@ -39,44 +33,9 @@ export function Orders() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Button>
-                    <Search className="h-3 w-3" />
-                    <span className="sr-only">Detalhes do pedido</span>
-                  </Button>
-                </TableCell>
-                <TableCell className="font-mono text-sm font-medium">
-                  3541843198431
-                </TableCell>
-                <TableCell className="text-muted-foreground">
-                  há 15 minutos
-                </TableCell>
-                <TableCell>
-                  <div className="flex items-center gap-2">
-                    <span className="h-2 w-2 rounded-full bg-slate-400" />
-                    <span className="font-medium text-muted-foreground">
-                      Pendente
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell className="font-medium">
-                  Jhonanthan Campos Xavier
-                </TableCell>
-                <TableCell className="font-medium"> R$ 149,90</TableCell>
-                <TableCell className="">
-                  <Button variant="outline" size="sm">
-                    <ArrowRight className="mr-2 h-3 w-3" />
-                    Aprovar
-                  </Button>
-                </TableCell>
-                <TableCell>
-                  <Button variant="ghost" size="sm">
-                    <X className="mr-2 h-3 w-3" />
-                    Cancelar
-                  </Button>
-                </TableCell>
-              </TableRow>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return <OrderTableRow key={i} />;
+              })}
             </TableBody>
           </Table>
         </div>

738af2b078b287ad9692477778cd8de1d084fa68
Jhonanthan <dionathancampos@hotmail.com>
2024-09-05 14:02:47 -0300

    Filtros dos pedidos parte 1

--- pizzashop-web/src/pages/app/orders/order-table-filters.tsx
+++ pizzashop-web/src/pages/app/orders/order-table-filters.tsx
@@ -7,27 +7,86 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Search, X } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { useSearchParams } from "react-router-dom";
+import { z } from "zod";
+
+const orderFiltersSchema = z.object({
+  orderId: z.string().optional(),
+  customerName: z.string().optional(),
+  status: z.string().optional(),
+});
+
+type OrderFiltersSchema = z.infer<typeof orderFiltersSchema>;
 
 export function OrderTableFilters() {
+  const [searchParams, setSerarchParams] = useSearchParams();
+
+  const orderId = searchParams.get("orderId");
+  const customerName = searchParams.get("customerName");
+  const status = searchParams.get("status");
+
+  const { register, handleSubmit, control } = useForm<OrderFiltersSchema>({
+    resolver: zodResolver(orderFiltersSchema),
+    defaultValues: {
+      orderId: orderId ?? "",
+      customerName: customerName ?? "",
+      status: status ?? "all",
+    },
+  });
+
+  function handleFilter({
+    orderId,
+    customerName,
+    status,
+  }: OrderFiltersSchema) {}
+
   return (
-    <form action="" className="flex items-center gap-2">
+    <form
+      onSubmit={handleSubmit(handleFilter)}
+      className="flex items-center gap-2"
+    >
       <span className="text-sm font-semibold">Filtros:</span>
-      <Input placeholder="Id Do Pedido" className="h-8 w-auto" />
-      <Input placeholder="Nome Do Cliente" className="h-8 w-[320px]" />
-      <Select defaultValue="all">
-        <SelectTrigger className="h-8 w-[180px]">
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="all">Todos estados</SelectItem>
-          <SelectItem value="pending">Pendente</SelectItem>
-          <SelectItem value="canceled">Cancelado</SelectItem>
-          <SelectItem value="processing">Em preparo </SelectItem>
-          <SelectItem value="delivering">Em entrega </SelectItem>
-          <SelectItem value="delivered">Entregue </SelectItem>
-        </SelectContent>
-      </Select>
+      <Input
+        placeholder="Id Do Pedido"
+        className="h-8 w-auto"
+        {...register("orderId")}
+      />
+      <Input
+        placeholder="Nome Do Cliente"
+        className="h-8 w-[320px]"
+        {...register("customerName")}
+      />
+
+      <Controller
+        name="status"
+        control={control}
+        render={({ field: { name, onChange, value, disabled } }) => {
+          return (
+            <Select
+              defaultValue="all"
+              name={name}
+              onValueChange={onChange}
+              value={value}
+              disabled={disabled}
+            >
+              <SelectTrigger className="h-8 w-[180px]">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">Todos estados</SelectItem>
+                <SelectItem value="pending">Pendente</SelectItem>
+                <SelectItem value="canceled">Cancelado</SelectItem>
+                <SelectItem value="processing">Em preparo </SelectItem>
+                <SelectItem value="delivering">Em entrega </SelectItem>
+                <SelectItem value="delivered">Entregue </SelectItem>
+              </SelectContent>
+            </Select>
+          );
+        }}
+      ></Controller>
 
       <Button type="submit" variant="secondary" size="sm">
         <Search className="mr-2 h-4 w-4" />

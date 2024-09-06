89d1e129bf8c0cb442712a9b23aa3024b4f9d83b
Jhonanthan Campos <dionathancampos@hotmail.com>
2024-09-06 16:37:02 -0300

    configurando logout automático

--- pizzashop-web/src/pages/_layouts/app.tsx
+++ pizzashop-web/src/pages/_layouts/app.tsx
@@ -1,7 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, replace, useNavigate } from "react-router-dom";
 import { Header } from "@/components/header";
+import { useEffect } from "react";
+import { api } from "@/lib/axios";
+import { isAxiosError } from "axios";
 
 export function AppLayout() {
+  const navigate = useNavigate();
+
+  //listener que vai acompanhar todas requisições realizadas
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      //quando a requisição der algum erro vai lançar uma ação
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status;
+          const code = error.response?.data.code;
+
+          // se o erro bater com esses criterior, a pessoa será redicionada para sign-in
+          if (status === 401 && code === "UNAUTHORIZED") {
+            navigate("/sign-in", { replace: true });
+          }
+        }
+      },
+    );
+
+    return () => {
+      api.interceptors.response.eject(interceptorId);
+    };
+  }, [navigate]);
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />

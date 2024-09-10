34e99dd8f5d143c6a55a74d4a8a77fbc3e10f174
Jhonanthan Campos <dionathancampos@hotmail.com>
2024-09-10 16:43:38 -0300

    Teste unitário de Pagination (completo)

--- pizzashop-web/src/components/pagination.spec.tsx
+++ pizzashop-web/src/components/pagination.spec.tsx
@@ -2,9 +2,15 @@ import { render } from "@testing-library/react";
 import { Pagination } from "./pagination";
 import { userEvent } from "@testing-library/user-event";
 
+//spy criado para os testes
 const onPageChangeCallback = vi.fn();
 
 describe("Pagination", () => {
+  // Antes de cada teste, o spy será limpo, para não haver interferências
+  beforeEach(() => {
+    onPageChangeCallback.mockClear();
+  });
+
   it("Should display the right amount of pages and results", () => {
     const wrapper = render(
       <Pagination
@@ -16,10 +22,10 @@ describe("Pagination", () => {
     );
 
     expect(wrapper.getByText("Página 1 de 20")).toBeInTheDocument();
-    expect(wrapper.getByText("Total de 200 items(s)")).toBeInTheDocument();
+    expect(wrapper.getByText("Total de 200 item(s)")).toBeInTheDocument();
   });
 
-  it("Should be able to navigate tothe next page", async () => {
+  it("Should be able to navigate to the next page", async () => {
     const wrapper = render(
       <Pagination
         pageIndex={0}
@@ -30,12 +36,76 @@ describe("Pagination", () => {
     );
 
     const nextPageButton = wrapper.getByRole("button", {
-      name: "Próxima página",
+      name: "Próxima Página",
     });
 
     const user = userEvent.setup();
     await user.click(nextPageButton);
 
-    expect(onPageChangeCallback).toHaveBeenCalled();
+    expect(onPageChangeCallback).toHaveBeenCalledWith(1);
+  });
+
+  it("Should be able to navigate to the previous page", async () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    );
+
+    const previousPageButton = wrapper.getByRole("button", {
+      name: "Página Anterior",
+    });
+
+    const user = userEvent.setup();
+    await user.click(previousPageButton);
+
+    expect(onPageChangeCallback).toHaveBeenCalledWith(4);
+  });
+
+  it("Should be able to navigate to the first page", async () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    );
+
+    const previousPageButton = wrapper.getByRole("button", {
+      name: "Primeira Página",
+    });
+
+    const user = userEvent.setup();
+    await user.click(previousPageButton);
+
+    expect(onPageChangeCallback).toHaveBeenCalledWith(0);
+  });
+
+  it("Should be able to navigate to the last page", async () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    );
+
+    const previousPageButton = wrapper.getByRole("button", {
+      name: "Última Página",
+    });
+
+    const user = userEvent.setup();
+
+    //verificando as chamada existentes no onPageChangeCallback,
+    //não deve haver nenhuma ja que ele esta sendo limpo antes de cada chamada
+    //console.log(onPageChangeCallback.mock.calls);
+    await user.click(previousPageButton);
+
+    expect(onPageChangeCallback).toHaveBeenCalledWith(19);
   });
 });

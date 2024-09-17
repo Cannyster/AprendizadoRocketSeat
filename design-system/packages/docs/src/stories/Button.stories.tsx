645d8c8fd6b55b3fc59a7a956fa349b46228d443
Jhonanthan <dionathancampos@hotmail.com>
2024-09-17 14:03:19 -0300

    realizar mais ajustes

---
+++ design-system/packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,9 @@
+import { StoryObj, Meta } from "@storybook/react";
+import { Button, ButtonProps } from "@ignite-ui/react";
+
+export default {
+  title: "Button",
+  Component: Button,
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj<ButtonProps> = {};

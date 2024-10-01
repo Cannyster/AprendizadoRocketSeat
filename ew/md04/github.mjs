75f404c2d05e352c7adc1608d986833e02bab65f
Jhonanthan <dionathancampos@hotmail.com>
2024-10-01 09:24:56 -0300

    extraindo dados do github

---
+++ ew/md04/github.mjs
@@ -0,0 +1,24 @@
+
+const userName = 'cannyster'
+
+fetch(`https://api.github.com/users/${userName}`, {
+    method: 'GET',
+    headers: {
+        Accept: 'application/vnd.github.v3+json',
+    },
+})
+    .then((response) => {
+        //console.log(typeof response)
+        //console.log(response)
+        return response.json()
+    })
+    .then((data) => {
+        console.log(`Nome: ${data.name}`)
+        console.log(`Login: ${data.login}`)
+        console.log(`Url: ${data.url}`)
+        console.log(`Company: ${data.company}`)
+        console.log(`Bio: ${data.bio}`)
+    })
+    .catch((err) => {
+        console.log(`Houve algum erro: ${err}`)
+    })
